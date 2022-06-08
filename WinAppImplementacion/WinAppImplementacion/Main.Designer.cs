828094812aa61b7774e378e0affd27dea4fac9e9
luisedr98 <50051344+luisedr98@users.noreply.github.com>
2022-06-07 19:35:59 -0500

    Add files via upload

---
+++ WinAppImplementacion/WinAppImplementacion/Main.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WinAppImplementacion
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PbxPantalla = new System.Windows.Forms.PictureBox();
+            this.BtnInterpolar = new System.Windows.Forms.Button();
+            this.BtnInterpolarHorizontal = new System.Windows.Forms.Button();
+            this.BnEjes3D = new System.Windows.Forms.Button();
+            this.BtnSuperficie3D = new System.Windows.Forms.Button();
+            this.BtnResorte = new System.Windows.Forms.Button();
+            this.BtnTiroParabolico = new System.Windows.Forms.Button();
+            this.BtnEjes2D = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PbxPantalla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PbxPantalla
+            // 
+            this.PbxPantalla.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.PbxPantalla.Location = new System.Drawing.Point(56, 12);
+            this.PbxPantalla.Name = "PbxPantalla";
+            this.PbxPantalla.Size = new System.Drawing.Size(700, 420);
+            this.PbxPantalla.TabIndex = 0;
+            this.PbxPantalla.TabStop = false;
+            this.PbxPantalla.Click += new System.EventHandler(this.PbxPantalla_Click);
+            // 
+            // BtnInterpolar
+            // 
+            this.BtnInterpolar.Location = new System.Drawing.Point(56, 464);
+            this.BtnInterpolar.Name = "BtnInterpolar";
+            this.BtnInterpolar.Size = new System.Drawing.Size(93, 23);
+            this.BtnInterpolar.TabIndex = 1;
+            this.BtnInterpolar.Text = "Interpolar V";
+            this.BtnInterpolar.UseVisualStyleBackColor = true;
+            this.BtnInterpolar.Click += new System.EventHandler(this.BtnInterpolar_Click);
+            // 
+            // BtnInterpolarHorizontal
+            // 
+            this.BtnInterpolarHorizontal.Location = new System.Drawing.Point(165, 465);
+            this.BtnInterpolarHorizontal.Name = "BtnInterpolarHorizontal";
+            this.BtnInterpolarHorizontal.Size = new System.Drawing.Size(88, 23);
+            this.BtnInterpolarHorizontal.TabIndex = 2;
+            this.BtnInterpolarHorizontal.Text = "Interpolar H";
+            this.BtnInterpolarHorizontal.UseVisualStyleBackColor = true;
+            this.BtnInterpolarHorizontal.Click += new System.EventHandler(this.BtnInterpolarHorizontal_Click);
+            // 
+            // BnEjes3D
+            // 
+            this.BnEjes3D.Location = new System.Drawing.Point(274, 464);
+            this.BnEjes3D.Name = "BnEjes3D";
+            this.BnEjes3D.Size = new System.Drawing.Size(75, 23);
+            this.BnEjes3D.TabIndex = 3;
+            this.BnEjes3D.Text = "Ejes 3D";
+            this.BnEjes3D.UseVisualStyleBackColor = true;
+            this.BnEjes3D.Click += new System.EventHandler(this.BnEjes3D_Click);
+            // 
+            // BtnSuperficie3D
+            // 
+            this.BtnSuperficie3D.Location = new System.Drawing.Point(366, 464);
+            this.BtnSuperficie3D.Name = "BtnSuperficie3D";
+            this.BtnSuperficie3D.Size = new System.Drawing.Size(92, 23);
+            this.BtnSuperficie3D.TabIndex = 4;
+            this.BtnSuperficie3D.Text = "Superficie3D";
+            this.BtnSuperficie3D.UseVisualStyleBackColor = true;
+            this.BtnSuperficie3D.Click += new System.EventHandler(this.BtnSuperficie3D_Click);
+            // 
+            // BtnResorte
+            // 
+            this.BtnResorte.Location = new System.Drawing.Point(483, 465);
+            this.BtnResorte.Name = "BtnResorte";
+            this.BtnResorte.Size = new System.Drawing.Size(75, 23);
+            this.BtnResorte.TabIndex = 5;
+            this.BtnResorte.Text = "Resorte";
+            this.BtnResorte.UseVisualStyleBackColor = true;
+            this.BtnResorte.Click += new System.EventHandler(this.BtnResorte_Click);
+            // 
+            // BtnTiroParabolico
+            // 
+            this.BtnTiroParabolico.Location = new System.Drawing.Point(584, 466);
+            this.BtnTiroParabolico.Name = "BtnTiroParabolico";
+            this.BtnTiroParabolico.Size = new System.Drawing.Size(75, 23);
+            this.BtnTiroParabolico.TabIndex = 6;
+            this.BtnTiroParabolico.Text = "Tiro";
+            this.BtnTiroParabolico.UseVisualStyleBackColor = true;
+            this.BtnTiroParabolico.Click += new System.EventHandler(this.BtnTiroParabolico_Click);
+            // 
+            // BtnEjes2D
+            // 
+            this.BtnEjes2D.Location = new System.Drawing.Point(59, 508);
+            this.BtnEjes2D.Name = "BtnEjes2D";
+            this.BtnEjes2D.Size = new System.Drawing.Size(75, 23);
+            this.BtnEjes2D.TabIndex = 7;
+            this.BtnEjes2D.Text = "Ejes 2D";
+            this.BtnEjes2D.UseVisualStyleBackColor = true;
+            this.BtnEjes2D.Click += new System.EventHandler(this.BtnEjes2D_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(835, 545);
+            this.Controls.Add(this.BtnEjes2D);
+            this.Controls.Add(this.BtnTiroParabolico);
+            this.Controls.Add(this.BtnResorte);
+            this.Controls.Add(this.BtnSuperficie3D);
+            this.Controls.Add(this.BnEjes3D);
+            this.Controls.Add(this.BtnInterpolarHorizontal);
+            this.Controls.Add(this.BtnInterpolar);
+            this.Controls.Add(this.PbxPantalla);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.PbxPantalla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox PbxPantalla;
+        private Button BtnInterpolar;
+        private Button BtnInterpolarHorizontal;
+        private Button BnEjes3D;
+        private Button BtnSuperficie3D;
+        private Button BtnResorte;
+        private Button BtnTiroParabolico;
+        private Button BtnEjes2D;
+    }
+}
---
+++ WinAppImplementacion/WinAppImplementacion/Main.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppImplementacion
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInterpolar_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(700, 420);
+            for (int i = 0; i < 700; i++)
+            {
+                for (int j = 0; j < 420; j++)
+                {
+                    if (i>=350)
+                    {
+                        int r = ((255 * (j - 420)) / -420) + ((240 * (j)) / 420);
+                        int g = ((165 * (j - 420)) / -420) + ((230 * (j)) / 420);
+                        int b = (140 * (j)) / 420;
+                        bmp.SetPixel(i, j, Color.FromArgb(r, g, b));
+                    }
+                    
+                   
+                }
+            }
+            PbxPantalla.Image = bmp;
+        }
+
+        private void BtnInterpolarHorizontal_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(700, 420);
+            for (int i = 0; i < 700; i++)
+            {
+                for (int j = 0; j < 420; j++)
+                {
+                    int r = ((255 * (i - 700)) / -700) + ((240 * (i)) / 700);
+                    int g = ((165 * (i - 700)) / -700) + ((230 * (i)) / 700);
+                    int b = (140 * (i)) / 700;
+                    bmp.SetPixel(i, j, Color.FromArgb(r,g,b));
+                }
+            }
+            PbxPantalla.Image = bmp;
+        }
+
+        private void BnEjes3D_Click(object sender, EventArgs e)
+        {
+            Color c = Color.Blue;
+            Segmento3D s = new Segmento3D(0, 0, 0, 0, 0, 5, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+            s = new Segmento3D(0, 0, 0, 5, 0, 0, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+            s = new Segmento3D(0, 8, 0, 0, 0, 0, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+
+            //ejes 8
+            s = new Segmento3D(0, 8, 0, 0, 5, 5, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+            //eje 9
+            s = new Segmento3D(8, 8, 0, 0, 0, 5, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+            //eje 4
+            s = new Segmento3D(8, 8, 0, 5, 0, 0, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+            //eje 5
+            s = new Segmento3D(0, 8, 5, 5, 0, 0, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+            //eje 7
+            s = new Segmento3D(0, 0, 0, 5, 5, 5, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+            //eje 6
+            s = new Segmento3D(0, 0, 5, 5, 0, 5, PbxPantalla);
+            PbxPantalla.Image = s.Encender(c);
+        }
+
+        private void BtnSuperficie3D_Click(object sender, EventArgs e)
+        {
+            Vector3D v = new Vector3D(PbxPantalla);
+            double t = -5;
+            double h;
+            do
+            {
+                h = -4;
+                do
+                {
+                    //tiene dos de radio
+                    double Xo = t;
+                    double Yo = h;
+                    double Zo = (0.17 * (Math.Pow(t, 2) + Math.Pow(h, 2))) - 3;
+                    v.Encender(Xo, Yo, Zo, Color.Red);
+                    h = h + 0.1;
+                } while (h <= 4);
+                t = t + 0.1;
+            } while (t <= 5);
+            PbxPantalla.Image = v.bmp;
+        }
+
+        private void BtnResorte_Click(object sender, EventArgs e)
+        {
+            Vector3D v = new Vector3D(PbxPantalla);
+            double Xo, Yo, Zo;
+            double h = 0;
+            do
+            {
+                //tiene dos de radio 
+                //la primera coordena en X es la pos 
+                //la primera coordena en Y es la pos
+                Xo = 1 + 2 * Math.Cos(h);
+                Yo = 2 + 2 * Math.Sin(h);
+                Zo = -4 + (h / 3); //separación entre cada vuelta (helongacion)
+
+                v.Encender(Xo, Yo, Zo, Color.Red);
+
+                h = h + 0.02;
+            } while (h <=30); //numero de vueltas
+            PbxPantalla.Image = v.Grafico();
+        }
+
+        private void BtnTiroParabolico_Click(object sender, EventArgs e)
+        {
+            Circunferencia c; 
+            Vector v = new Vector(PbxPantalla);
+            double t = -8;
+            do
+            {
+                int xo = v.PantallaSx(t);
+                int yo = v.PantallaSy(((-Math.Pow(t, 2) - (12* t) - 32 )));
+                v.Encender(xo, yo, Color.Blue);
+                t = t + 0.001;
+            } while (t <= -4);
+            PbxPantalla.Image = v.bmp;
+
+            t = -4;
+            do
+            {
+                int xo = v.PantallaSx(t);
+                int yo = v.PantallaSy(((-Math.Pow(t, 2) - (3 * t) + 4)/1.06));
+                v.Encender(xo, yo, Color.Blue);
+                t = t + 0.001;
+            } while (t <= 1);
+            PbxPantalla.Image = v.bmp;
+
+
+            //t = -8;
+            //double radio = 0.5;
+            //do
+            //{
+            //    double xo = (t);
+            //    double yo = ((-Math.Pow(t, 2) - (12 * t) - 32));
+            //    c = new Circunferencia(xo, yo, radio, PbxPantalla);
+            //    PbxPantalla.Image = c.Encender(Color.Green);
+            //    PbxPantalla.Refresh();
+            //    Thread.Sleep(5);
+            //    PbxPantalla.Image = c.Apagar(PbxPantalla);
+            //    t = t + 0.1;
+            //    c = null;
+            //} while (t <= -4);
+        }
+
+        private void BtnEjes2D_Click(object sender, EventArgs e)
+        {
+            Segmento s = new Segmento(-10, 10, 0, 0, PbxPantalla);
+            PbxPantalla.Image = s.Encender(Color.Red);
+            s = new Segmento(0, 0, 5.98, -5.98, PbxPantalla);
+            PbxPantalla.Image = s.Encender(Color.Red);
+        }
+
+        private void PbxPantalla_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
